128a24e9353e4e3c98b6e683405fb105f5d1183c
kazu-oku1 <hawai.waikiki.oahu7@gmail.com>
2022-07-09 21:25:55 +0900

    編集、削除機能実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,5 @@
 class TweetsController < ApplicationController
+  before_action :set_prototype, except: [:index, :new, :create]
 
   def index
     @tweets = Tweet.includes(:user)
@@ -17,11 +18,35 @@ class TweetsController < ApplicationController
     end
   end
 
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @tweet.update(tweet_params)
+      redirect_to tweet_path(@tweet)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @tweet.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
+
   private
 
   def tweet_params
     params.require(:tweet).permit(:title, :material, :guide, :recipe, :image).merge(user_id: current_user.id)
   end
 
-
+  def set_prototype
+    @tweet = Tweet.find(params[:id])
+  end
 end
